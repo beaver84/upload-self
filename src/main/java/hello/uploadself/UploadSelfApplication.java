a9ab22e3b987b7565b638312e1b04cadb8e7155a
Kyungtae Bae <Kyungtae@marvrus.com>
2022-09-04 21:06:21 +0900

    Initial commit

---
+++ src/main/java/hello/uploadself/UploadSelfApplication.java
@@ -0,0 +1,13 @@
+package hello.uploadself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadSelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/uploadself/UploadSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.uploadself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
